491b77670f55022f27b0fb002a2873c07f743634
Dhruv <agrawaldhruv1006@gmail.com>
2023-01-03 09:42:46 +0530

    Added some server code

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+server.listen(port , "0.0.0.0" , () => {
+    console.log(`Server started and running on port , ${port}`);
+});
